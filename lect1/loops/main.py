e87c61c04e08d9f6a856dfa1c77f0b5d86324535
evgen <evgeny_vlasov@yahoo.com>
2020-04-20 12:22:57 +0300

    added while for loops

---
+++ lect1/loops/main.py
@@ -0,0 +1,18 @@
+# ЦИКЛ - ЛОКАЛЬНО ИСПОЛНЯЕМЫЙ БЛОК КОДА, который выполняется определенное количество раз.
+
+# while expression:
+#     body 
+
+# for expression:
+#     body 
+
+i = 0 
+while i < 11: # <= 10
+    print("Current value:", i)
+    i = i + 1
+
+a = 20
+a = a + 30 # Составной оператор присваивания
+a += 1 #a = a + 1 
+#-=, *=, /=, **=, //=, %=
+print("AFTER WHILE Loop")
---
+++ lect1/loops/main2.py
@@ -0,0 +1,9 @@
+for i in range(0, 5, 1): #range(start=0, stop,step=1)
+    print("Current value:", i)
+
+for i in range(20, 2, -1):
+    print("Current value:", i)
+
+
+if "a" in "cat":
+    print("YES a in")
